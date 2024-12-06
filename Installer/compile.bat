c010434c11d4d4d6c52ebb9b2457b6d9b4cbd9e1
portex7000 <michalstaffen55@gmail.com>
2024-12-06 18:57:00 +0100

    modified:   Installer/compile.bat new file:   Installer/installer.manifest

--- Installer/compile.bat
+++ Installer/compile.bat
@@ -1,2 +1,3 @@
+windres installer.manifest -O coff -o manifest.res
 windres installer.rc -O coff -o installer.res
-gcc installer.c installer.res -o installer.exe -mwindows
+gcc installer.c installer.res manifest.res -o installer.exe -mwindows
