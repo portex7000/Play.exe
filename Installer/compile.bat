aed848072253c01f2d9f7a384b53ed9b7de9eecf
portex7000 <michalstaffen55@gmail.com>
2024-12-06 19:09:13 +0100

    modified:   Installer/compile.bat modified:   Installer/installer.manifest deleted:    Installer/installer.res

--- Installer/compile.bat
+++ Installer/compile.bat
@@ -1,3 +1,4 @@
-windres installer.manifest -O coff -o manifest.res
+:: Please fix the installer.manifest file
+:: windres installer.manifest -O coff -o manifest.res
 windres installer.rc -O coff -o installer.res
 gcc installer.c installer.res manifest.res -o installer.exe -mwindows
